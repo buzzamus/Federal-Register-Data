794652a4e6ad298af2154cb8f91c512e8ee4544d
buzzamus <brent.busby@yahoo.com>
2023-04-04 19:22:13 -0500

    remove unneccesary comments

--- FederalRegisterData/Configuration.swift
+++ FederalRegisterData/Configuration.swift
@@ -12,6 +12,3 @@ struct Configuration {
     static let latestArticlesEndpoint = "https://www.federalregister.gov/api/v1/documents.json?fields[]=body_html_url&fields[]=document_number&fields[]=title&per_page=10&conditions[agencies][]="
     static let failedNetworkRequestText = "There was an error retrieving the requested data.\n Either the service is down, or you are not connected to wifi or a mobile network.\n Try again later."
 }
-
-// make call on AgencyView to latestArticlesEndpoint with agency-slug at end
-// then, for each create link to article to link to for browser view
